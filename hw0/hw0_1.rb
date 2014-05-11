9ae1593906a53136413c64ea1281bd3694d0dc32
Neil Thompson <neil@white-label-development.co.uk>
2014-05-11 19:55:52 +0100

    snap

--- hw0/hw0_1.rb
+++ hw0/hw0_1.rb
@@ -2,17 +2,26 @@
 # and returns the sum of its elements. For an empty array it should return zero.
 
 def sum(ints)
-  ints.empty?? 0: 1
+  sum_of_all = 0
+  if ints.empty? 
+    return 0 
+  else
+  	ints.each{|i| sum_of_all+=i}
+  	sum_of_all
+  end
 end
 
 
 #copying sam's TDD stylee
 raise "An empty array should return zero" unless sum([]) == 0
-
+raise "An empty array should return the sum of it's elements" unless sum([1,2,3,]) == 6
+raise "An empty array should return the sum of it's elements" unless sum([1,2,3,4]) == 10
 # Define a method max_2_sum which takes an array of integers as an argument 
 #and returns the sum of its two largest elements. 
 #For an empty array it should return zero. 
 #For an array with just one element, it should return that element.
+
+
 
 # Define a method sum_to_n? which takes an array of integers 
 #and an additional integer, n, as arguments 
